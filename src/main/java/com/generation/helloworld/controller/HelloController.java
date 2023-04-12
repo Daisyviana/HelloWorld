4e8f7de807f1b5888aa0d9b5b578a0681ef98d42
Daisy Kelly Viana <daisyviana96@gmail.com>
2023-04-12 15:52:39 -0300

    BSM’s da Generation

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -7,14 +7,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
-	
+
 	@GetMapping
 	public String hello() {
 		return "<b>Hello Turma 62!</b>";
 	}
-	
+
 	@GetMapping("/nome")
 	public String meuNome() {
 		return "<b>Daisy Viana!</b>";
+	}
+
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "<b>Responsabilidade Pessoal<br />" + "Mentalidade de Crescimento<br />" + "Orientação ao Futuro<br />"
+				+ "Persistência<br />" + "Comunicação<br />" + "Orientação aos Detalhes<br />"
+				+ "Trabalho em Equipe<br />" + "Proatividade</b>";
+	}
+
+	@GetMapping("/objetivos")
+	public String objetivos() {
+		return "<b>Objetivos<br />" + "- Apender mais sobre spring<br />"
+				+ "- Colher todos os conhecimentos sobre o MySql"; 
 	}
 }
